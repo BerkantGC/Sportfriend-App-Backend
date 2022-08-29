b00c13620e08b126486ecd9d83d728876d58e7be
BerkantGC <berkant103x@hotmail.com>
2022-08-29 11:54:43 +0300

    Deployed to Heroku

---
+++ src/main/java/com/GameSatis/backend/repository/MessagesRepository.java
@@ -0,0 +1,10 @@
+package com.GameSatis.backend.repository;
+
+import com.GameSatis.backend.model.MessageData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessagesRepository extends JpaRepository<MessageData, Long> {
+    List<MessageData> findAllByReceiverNameAndAndSenderName(String receiverName, String senderName);
+}
